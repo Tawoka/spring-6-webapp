c7d132f1763fd1df43a0b07af2893c491bd50393
tawoka <github@spectral-darkness.org>
2023-05-09 09:41:31 +0200

    Adding BookService

---
+++ src/main/java/com/rmorgner/spring6webapp/services/BookService.java
@@ -0,0 +1,9 @@
+package com.rmorgner.spring6webapp.services;
+
+import com.rmorgner.spring6webapp.domain.Book;
+
+public interface BookService {
+
+  Iterable<Book> getAll();
+
+}
---
+++ src/main/java/com/rmorgner/spring6webapp/services/BookServiceImpl.java
@@ -0,0 +1,20 @@
+package com.rmorgner.spring6webapp.services;
+
+import com.rmorgner.spring6webapp.domain.Book;
+import com.rmorgner.spring6webapp.repositories.BookRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+  private final BookRepository bookRepository;
+
+  public BookServiceImpl(BookRepository bookRepository) {
+    this.bookRepository = bookRepository;
+  }
+
+  @Override
+  public Iterable<Book> getAll() {
+    return bookRepository.findAll();
+  }
+}
